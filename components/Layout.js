d532ddfdab2e49b0f6d726047ff2b8da2a9df937
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-04-17 16:04:03 -0700

    Add product to cart functionality

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,12 @@
+import { Store } from '@/utils/Store'
 import Head from 'next/head'
 import Link from 'next/link'
+import { useContext } from 'react'
 
 export default function Layout({ title, children }) {
+  const { state } = useContext(Store)
+  const { cart } = state
+
   return (
     <>
       <Head>
@@ -11,19 +16,30 @@ export default function Layout({ title, children }) {
       </Head>
       <div className="flex min-h-screen flex-col justify-between">
         <header>
-          <nav className='flex h-12 items-center px-4 justify-between shadow-md'>
+          <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/" className="text-lg font-bold">
               amazona
             </Link>
             <div>
-              <Link href="/cart" className='p-2'>Cart</Link>
-              <Link href="/login" className='p-2'>Login</Link>
+              <Link href="/cart" className="p-2">
+                Cart
+                {cart.cartItems.length > 0 && (
+                  <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
+                    {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                  </span>
+                )}
+              </Link>
+              <Link href="/login" className="p-2">
+                Login
+              </Link>
             </div>
           </nav>
         </header>
-        <main className='container m-auto mt-4 px-4'>{children}</main>
-        <footer className='flex h-10 justify-center items-center shadow-inner'>Copyright © 2023 Amazona</footer>
+        <main className="container m-auto mt-4 px-4">{children}</main>
+        <footer className="flex h-10 justify-center items-center shadow-inner">
+          Copyright © 2023 Amazona
+        </footer>
       </div>
     </>
-  );
+  )
 }
