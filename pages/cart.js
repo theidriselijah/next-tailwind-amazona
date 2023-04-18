d6726dfdd5a0b137f9706d3a34261d502bd42a5b
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-04-18 14:28:57 -0700

    Implemented cookies for cart items

--- pages/cart.js
+++ pages/cart.js
@@ -5,8 +5,9 @@ import Link from 'next/link'
 import { useContext } from 'react'
 import { XCircleIcon } from '@heroicons/react/24/outline'
 import { useRouter } from 'next/router'
+import dynamic from 'next/dynamic'
 
-export default function CartScreen() {
+function CartScreen() {
   const router = useRouter()
   const { state, dispatch } = useContext(Store)
   const {
@@ -107,3 +108,5 @@ export default function CartScreen() {
     </Layout>
   )
 }
+
+export default dynamic(() => Promise.resolve(CartScreen), { ssr: false })
--- pages/index.js
+++ pages/index.js
@@ -3,11 +3,13 @@ import ProductItem from '@/components/ProductItem'
 import data from '../utils/data'
 
 export default function Home() {
-  return <Layout title="Homepage">
-    <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
-      {data.products.map(product => (
-        <ProductItem product={product} key={product.slug}></ProductItem>
-      ))}
-    </div>
-  </Layout>
+  return (
+    <Layout title="Homepage">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  )
 }
