5c76c889f933620ee1b592ff0d26014683c1b67d
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-04-19 01:33:52 -0700

    Added user authentication

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '@/styles/globals.css'
+import { SessionProvider } from 'next-auth/react'
 import { StoreProvider } from '@/utils/Store'
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-  <StoreProvider>
-    <Component {...pageProps} />
-  </StoreProvider>
-  ); 
+    <SessionProvider session={session}>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </SessionProvider>
+  )
 }
