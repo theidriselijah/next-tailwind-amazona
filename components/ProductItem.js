818988044f453f0906a53daa24e67a388323732c
theidriselijah <107328693+theidriselijah@users.noreply.github.com>
2023-04-20 19:39:35 -0700

    Load products from MongoDB

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,25 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
 import Link from 'next/link'
 
-export default function ProductItem({ product }) {
+export default function ProductItem({ product, addToCartHandler }) {
   return (
-    <div className='card'>
+    <div className="card">
       <Link href={`/product/${product.slug}`}>
         <img
-            src={product.image}
-            alt={product.name}
-            className='rounded shadow' 
+          src={product.image}
+          alt={product.name}
+          className="rounded shadow"
         />
       </Link>
 
-      <div className='flex flex-col items-center justify-center p-5'>
+      <div className="flex flex-col items-center justify-center p-5">
         <Link href={`/product/${product.slug}`}>
-            <h2 className='text-lg'>{product.name}</h2>
+          <h2 className="text-lg">{product.name}</h2>
         </Link>
-        <p className='mb-2'>{product.brand}</p>
+        <p className="mb-2">{product.brand}</p>
         <p>${product.price}</p>
-        <button className="primary-button" type="button">
-            Add to cart
+        <button
+          className="primary-button"
+          type="button"
+          onClick={() => addToCartHandler(product)}
+        >
+          Add to cart
         </button>
       </div>
     </div>
